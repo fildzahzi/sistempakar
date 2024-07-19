8b0ad2a171db8a6617a09e272e2eb9212303b1d2
fildzahzi <fildzahzata12@gmail.com>
2024-07-19 15:00:16 +0700

    sistem pakar naive bayes

---
+++ home.php
@@ -0,0 +1,5 @@
+<div class="page-header">
+    <h1 class="entry-title">Sistem Pakar Metode Bayes</h1>
+</div>
+<p class="entry-summary hidden">Sistem pakar metode Teorema Bayes berbasis web dengan PHP dan MySQL.  Studi kasus: mendiagnosa penyakit pada manusia melalui gejala yang dialami</p>
+<p class="entry-content">Dalam teori probabilitas dan statistika, teorema Bayes adalah sebuah teorema dengan dua penafsiran berbeda. Dalam penafsiran Bayes, teorema ini menyatakan seberapa jauh derajat kepercayaan subjektif harus berubah secara rasional ketika ada petunjuk baru. Dalam penafsiran frekuentis teorema ini menjelaskan representasi invers probabilitas dua kejadian. Teorema ini merupakan dasar dari statistika Bayes dan memiliki penerapan dalam sains, rekayasa, ilmu ekonomi (terutama ilmu ekonomi mikro), teori permainan, kedokteran dan hukum. Penerapan teorema Bayes untuk memperbarui kepercayaan dinamakan inferens Bayes.</p>
